986788fe1c20a641e4b28ebb8d8abf99e177dc1b
Andrea Bernardini <andrebask@gmail.com>
2013-10-31 11:15:29 +0100

    Preparing for publishing

--- src/AccountManagerWS/static/js/bookmarklet.js
+++
@@ -1,1 +0,0 @@
-javascript: (function () {var jsCode = document.createElement('script');jsCode.setAttribute('src', 'http://localhost:2000/static/js/submit.js');document.body.appendChild(jsCode);}());
--- src/UserInterface/NewsPrint/static/js/submit.js
+++ src/UserInterface/NewsPrint/static/js/submit.js
@@ -1,3 +1,19 @@
+////
+// Copyright (c) 2013 Andrea Bernardini.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+////
+
 function post_to_url(path, params, method) {
     method = method || "post"; // Set method to post by default if not specified.
 
